7d4e64454d406be485d7bc6f56b298c44cf2b676
rostikrff98 <sheremeta967@ukr.net>
2021-05-20 17:36:02 +0200

    final

--- src/main/java/ua/lviv/test/dao/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package ua.lviv.test.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ua.lviv.test.domain.Department;
-
-public interface DepartmentRepository extends JpaRepository<Department, Integer> {
-
-}
--- src/main/java/ua/lviv/test/domain/CategoriesNames.java
+++
@@ -1,5 +0,0 @@
-package ua.lviv.test.domain;
-
-public enum Degree {
-	ASSISTANT, ASSOCIATE_PROFESSOR, PROFESSOR
-}
